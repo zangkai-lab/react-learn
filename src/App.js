a9c9d222311665463dd0bdf4be264377a4fa112e
zangkai-lab <erickant505@gmail.com>
2023-09-15 17:30:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,42 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// JavaScript 的 export 关键字使此函数可以在此文件之外访问。default 关键字表明它是文件中的主要函数
+import { useState } from 'react';
+
+function Square({value}) {
+  // const [value, setValue] = useState(null);
+
+  // function handleClick() {
+  //   setValue("X");
+  // }
+
+  return (
+    <button 
+      className="square" 
+      // onClick={handleClick}
+    >
+      {value}
+    </button>);
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  return (    
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
 }
